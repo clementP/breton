446e7bce6acf3102864f1b4701f5c51ec53852ce
clement <clement.perrin.speights@gmail.com>
2012-09-12 15:23:20 +0200

    debut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breton::Application.config.secret_token = 'c79035a4b5f5b0ee7bc5f3fbab9da0fcf416cbbb4683504ac117d0f9638d18a04042f903d7a173b48d6943cd791c165f6eb448f4cb453a42e27739f940a178ab'
